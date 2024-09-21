fdda02dd44bc8a8829b0f737a67b0b1f1232b121
lil-aleks <olekiwan4@gmail.com>
2024-09-21 22:41:34 +0200

    Updated README.md and the test MainClass.

--- README.md
+++ README.md
@@ -1,1 +1,97 @@
-"# DcBHelper" 
+# DcBotHelper
+This will help you to build your own Discord bot faster!
+## Implement to your project
+``TODO``
+## Get started
+When creating a bot at the [Discord Developer Portal](https://discord.com/developers), please don´t forget to toggle every privileged gateway intent on.
+
+### Starting the bot
+#### Prepared method
+You can start your bot using this method:
+```java 
+Bot.startBot(YOUR_TOKEN_HERE); // Enter your token
+```
+#### Custom method
+Or create your own method and add the EventHandler to the EventLiteners.
+```java
+JDA bot = JDABuilder.createDefault(YOUR_TOKEN_HERE) // Enter your token
+    .setStatus(OnlineStatus.ONLINE) // optional
+    .setActivity(Activity.playing("nothing")) //optional
+    .setChunkingFilter(ChunkingFilter.ALL)
+    .setMemberCachePolicy(MemberCachePolicy.ALL)
+    .enableIntents(
+        GatewayIntent.GUILD_MEMBERS,
+        GatewayIntent.GUILD_PRESENCES,
+        GatewayIntent.GUILD_VOICE_STATES,
+        GatewayIntent.DIRECT_MESSAGE_TYPING,
+        GatewayIntent.DIRECT_MESSAGES,
+        GatewayIntent.MESSAGE_CONTENT,
+        GatewayIntent.GUILD_INVITES
+    )
+    .addEventListeners(
+        new EventHandler() // DON'T FORGET THIS LINE
+    )
+    .build().awaitReady();
+```
+### Events
+Here are some examples how to make some Events.
+#### Slash-Command interactions:
+Add `@OnCommand` with a `name` and `description` to a `public static` method and add `SlashCommandInteractionEvent event` as the parameter:
+```java
+// /ping command which response with "Pong"!
+@OnCommand(name = "ping", description = "ping the bot")
+public static void example(SlashCommandInteractionEvent event)
+{
+    event.reply("Pong!").setEphemeral(true).queue();
+}
+```
+You can also add options to the command by adding as many `@CmdOption` to `options` as you want:
+```java
+// /time command which response with the time a Member is joined the server!
+@OnCommand(name = "time", description = "when did a user  join!", options = {
+        @CmdOption(name = "user", description = "who?", type = OptionType.USER, required = true)
+})
+public static void slashCmd(SlashCommandInteractionEvent event)
+{
+    Member member = event.getOption("user").getAsMember();
+    event.reply(member.getAsMention() + " joined on " + member.getTimeJoined()).setEphemeral(true).queue();
+}
+```
+Those methods will automatically create a slash command and execute whenever the command is used.
+#### Button interactions:
+Add `@OnButton` with a `id`  to a `public static` method and add `ButtonInteractionEvent event` as the parameter:
+```java
+// whenever a member presses a button with the id "verify"
+// the member will get a specific role on the server.
+@OnButton(id = "verify")
+public static void verifyMember(ButtonInteractionEvent event)
+{
+    Member member = event.getMember();
+    Role verifyRole = event.getGuild().getRoleById("1234567891234567891");
+    event.getGuild().addRoleToMember(member, verifyRole).queue();
+    event.reply("You are now verified!").setEphemeral(true).queue();
+}
+```
+This method will execute whenever a button with that id is pressed.
+#### Message receive
+Add `@OnMessageReceived` on a `public static` method and add `MessageReceivedEvent event` as the parameter:
+```java
+// react with a :speech_balloon: when a messsage is received.
+// I really couldn't think of any other example.
+@OnMessageReceived()
+public static void reactToAnyMessage(MessageReceivedEvent event)
+{
+    event.getMessage().addReaction(Emoji.fromFormatted(":speech_balloon:")).queue();
+}
+```
+You can also make a method react to a specific message by adding `message`(Case is ignored in the string):
+```java
+// response whenever someone sends "Hello". (Case is ignored in the string)
+@OnMessageReceived(message = "hello")
+public static void responseOnHello(MessageReceivedEvent event)
+{
+    event.getMessage().reply("Hello " + event.getMember().getAsMention()).queue();
+}
+```
+This methods will execute every time someone sends a message.
+### MORE SOON.
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,8 +1,17 @@
-import com.bothelper.event.interaction.CmdOption;
-import com.bothelper.event.interaction.OnCommand;
+import com.bothelper.event.interaction.*;
 import com.bothelper.main.Bot;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.Widget;
+import net.dv8tion.jda.api.entities.channel.Channel;
+import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
+import net.dv8tion.jda.api.entities.channel.middleman.AudioChannel;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.EntitySelectInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 
 import java.io.BufferedReader;
@@ -25,12 +34,75 @@ public class MainTest
         Bot.startBot(token);
     }
 
+    // /ping command which response with "Pong"!
+    @OnCommand(name = "ping", description = "ping the bot")
+    public static void example(SlashCommandInteractionEvent event)
+    {
+        event.reply("Pong!").setEphemeral(true).queue();
+    }
+
+    // /time command which response with the time a Member is joined the server!
     @OnCommand(name = "time", description = "when did a user  join!", options = {
             @CmdOption(name = "user", description = "who?", type = OptionType.USER, required = true)
     })
     public static void slashCmd(SlashCommandInteractionEvent event)
     {
         Member member = event.getOption("user").getAsMember();
-        event.reply(member.getAsMention() + " joined on" + member.getTimeJoined()).setEphemeral(true).queue();
+        event.reply(member.getAsMention() + " joined on " + member.getTimeJoined()).setEphemeral(true).queue();
+    }
+
+    // whenever a member presses a button with the id "verify"
+    // the member will get a specific role on the server.
+    @OnButton(id = "verify")
+    public static void verifyMember(ButtonInteractionEvent event)
+    {
+        Member member = event.getMember();
+        Role verifyRole = event.getGuild().getRoleById("1234567891234567891");
+        event.getGuild().addRoleToMember(member, verifyRole).queue();
+        event.reply("You are now verified!").setEphemeral(true).queue();
+    }
+
+    // react with a :speech_balloon: when a messsage is received.
+    // I really couldn't think of any other example.
+    @OnMessageReceived()
+    public static void reactToAnyMessage(MessageReceivedEvent event)
+    {
+        event.getMessage().addReaction(Emoji.fromFormatted(":speech_balloon:")).queue();
+    }
+
+    // response whenever someone sends "Hello". (Case is ignored in the string)
+    @OnMessageReceived(message = "hello")
+    public static void responseOnHello(MessageReceivedEvent event)
+    {
+        event.getMessage().reply("Hello " + event.getMember().getAsMention()).queue();
+    }
+
+    // whenever someone select something from an EntitySelection
+    // with the id "move-user-channel" the user will move to
+    // the selected Voice-Channel.
+    @OnEntitySelection(id = "move-user-channel")
+    public static void moveUser(EntitySelectInteractionEvent event)
+    {
+        Channel channel = (Channel) event.getValues().get(0);
+        if (!(channel instanceof AudioChannel))
+        {
+            event.reply("You need to select a Voice-Channel.").setEphemeral(true).queue();
+            return;
+        }
+        if (!event.getMember().getVoiceState().inAudioChannel())
+        {
+            event.reply("You need to be in a Voice-Channel.").setEphemeral(true).queue();
+            return;
+        }
+        event.getGuild().moveVoiceMember(event.getMember(), (AudioChannel) channel).queue();
+    }
+
+    // whenever someone select something from a StringSelection
+    // with the id "favorite-fruit" the user will get a response
+    // that the bot also like the fruit the user chose.
+    @OnStringSelection(id = "favorite-fruit")
+    public static void selectedFavFruit(StringSelectInteractionEvent event)
+    {
+        event.reply(event.getValues().get(0) + " is my fav fruit too!").setEphemeral(true).queue();
     }
 }
